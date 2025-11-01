0657d77a059cc8937d8dca8892712ffbb2449503
jeffery-jefferson <mark.thomas.ureta@gmail.com>
2025-11-02 01:32:15 +1100

    more test cases

--- Runners/TestCaseRunner.java
+++ Runners/TestCaseRunner.java
@@ -3,10 +3,8 @@ package Runners;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
@@ -17,13 +15,19 @@ public class TestCaseRunner {
     private static TestCase[] tests = new TestCase[] {
         new TestCase("SIMPLE NUMBER", "1", "[1]"),
         new TestCase("SIMPLE IDENTIFIER", "x", "[x]"),
+        new TestCase("SIMPLE PAREN PLUS", "(+ 2 3)", "[[(, [+, 2, 3], )]]"),
+        new TestCase("SIMPLE PAREN MULTIPLY", "(× x 5)", "[[(, [×, x, 5], )]]"),
+        new TestCase("NESTED NORMAL", "(+ (× 2 3) 4)", "[[(, [+, [(, [×, 2, 3], )], 4], )]]"),
+        new TestCase("NESTED CONDITIONAL", "(? (= x 0) 1 0)", "[[(, [?, [(, [=, x, 0], )], 1, 0], )]]"),
+        new TestCase("LAMBDA", "(λ x x)", "[[(, [λ, x, x], )]]"),
+        new TestCase("LET", "(≜ y 10 y)", "[[(, [≜, y, 10, y], )]]"),
+        new TestCase("NESTED LAMBDA", "((λ x (+ x 1)) 5)", "[[(, [[(, [λ, x, [(, [+, x, 1], )]], )], 5], )]]"),
     };
 
     public static void Run() {
         var fileName = "testOutput.json";
-        var mapper = new ObjectMapper();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
         var testResults = new ArrayList<TestCase.TestCaseResult>();
+
         for (var test : tests) {
             var resultTree = Runner.Run(test.getInput().toString(), false);
             var result = simplify(resultTree.getRoot()).toString().trim();
@@ -33,6 +37,8 @@ public class TestCaseRunner {
         }
 
         try (var writer = new FileWriter(fileName)) {
+            var mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
             var results = mapper.writeValueAsString(testResults);
             writer.write(results);
         } catch (IOException ex) {
